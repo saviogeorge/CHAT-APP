8d821f521bebfade9fbe04e2826f698a7d0ec02a
savgeor <savio.george@mercedes-benz.com>
2025-03-27 21:03:35 +0100

    login page and nav bar updates

--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -1,5 +1,6 @@
 import {create} from "zustand"
 import { axiosInstance } from "../lib/axios";
+import { toast } from 'react-hot-toast';
 
 export const useAuthStore = create((set,) => ({
     authUser: null,
@@ -35,5 +36,29 @@ export const useAuthStore = create((set,) => ({
       }
     },
 
+    login: async (data) => {
+      set({ isLoggingIn: true });
+      try {
+        const res = await axiosInstance.post("/auth/login", data);
+        set({ authUser: res.data });
+        toast.success("Logged in successfully");
+  
+        get().connectSocket();
+      } catch (error) {
+        toast.error(error.response.data.message);
+      } finally {
+        set({ isLoggingIn: false });
+      }
+    },
+    
+    logout: async () => {
+      try {
+        await axiosInstance.post("/auth/logout");
+        set({ authUser: null });
+        toast.success("Logged out successfully");
+      } catch (error) {
+        toast.error(error.response.data.message);
+      }
+    },
     
 }));
